2c079c900a729d955b6c193460801fd35d5ec3a5
ninsy <kwojciechowski094@gmail.com>
2019-03-19 10:11:29 +0100

    balance, customers ready

--- voucherify/balance.py
+++ voucherify/balance.py
@@ -1,4 +1,12 @@
+try:
+    from urllib.parse import urlencode, quote
+except ImportError:
+    from urllib import urlencode
+    from urllib import quote
+
 class Balance:
 	def __init__(self, client):
 		self.client = client
-		self.url = '/balance'
+
+	def create(self, code, properties):
+		return self.client.post('/vouchers/{}/balance'.format(quote(code)), properties)
--- voucherify/customers.py
+++ voucherify/customers.py
@@ -1,4 +1,55 @@
+try:
+    from urllib.parse import urlencode, quote
+except ImportError:
+    from urllib import urlencode
+    from urllib import quote
+
 class Customers:
 	def __init__(self, client):
 		self.client = client
 		self.url = '/customers'
+
+	def get(self, customer_id):
+		"""
+			https://docs.voucherify.io/reference#read-customer
+		"""
+		return self.client.get('{}/{}'.format(self.url, quote(customer_id)))
+
+	def create(self, customer):
+		"""
+			https://docs.voucherify.io/reference#create-customer
+		"""
+		return self.client.post(self.url, customer)
+
+	def update(self, customer):
+		"""
+			https://docs.voucherify.io/reference#update-customer
+		"""
+		# hasattr(x, "__getitem__")
+		if isinstance(customer, dict):
+			encoding_key = customer.get('id') or customer.get('source_id')
+		else:
+			encoding_key = customer.id or customer.source_id
+		del customer['id']
+		return self.client.put('{}/{}'.format(self.url, quote(encoding_key)), customer)
+
+	def delete(self, customer_id):
+		"""
+			https://docs.voucherify.io/reference#delete-customer
+		"""
+		return self.client.delete('{}/{}/permanent-deletion'.format(self.url, quote(customer_id)))
+
+	def permanent_delete(self, customer_id):
+		"""
+			The organization user can remove consumer data permanently from the Voucherify system by using this API method.
+			It d​eletes all customer data and connected resources. It makes customer profile forgotten by Voucherify.
+			https://docs.voucherify.io/reference#delete-customer-permanently
+		"""
+		return self.client.delete('{}/{}'.format(self.url, quote(customer_id)))
+
+	def list_customers(self, params):
+		"""
+			Returns a list of your vouchers. The vouchers are returned sorted by creation date, with the most recent vouchers appearing first.
+			https://docs.voucherify.io/v2018-08-01/reference#list-vouchers
+		"""
+		return self.client.get(self.url, params)		
